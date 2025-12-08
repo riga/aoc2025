8061b3e5b6c0b633d5abae3d4e64c86e9f8bddd4
Marcel R. <github.riga@icloud.com>
2025-12-08 10:26:00 +0100

    Opt day 8.

--- aoc2025/day08.py
+++ aoc2025/day08.py
@@ -8,29 +8,33 @@ from __future__ import annotations
 
 import itertools
 
-from aoc2025 import Solver, Part, Point3
+from aoc2025 import Solver, Part
 
 
 def solution(data: list[str], part: Part) -> int | str | None:
     # create list of 3d points
-    points = [Point3(*map(int, line.split(","))) for line in data]
+    points = [tuple(map(int, line.split(","))) for line in data]
 
     # compute distance brute force for all combinations and sort
     closest_pairs = sorted(
-        ((p1, p2, p1.distance(p2)) for p1, p2 in itertools.combinations(points, 2)),
+        (
+            (p1, p2, ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)**0.5)
+            for p1, p2 in itertools.combinations(points, 2)
+        ),
         key=(lambda item: item[2]),
     )
 
     # create connections, keepin track of all clusters
-    clusters: dict[Point3, set[Point3]] = {point: {point} for point in points}
+    clusters: dict[tuple, set[tuple]] = {point: {point} for point in points}
     last_product = 0  # part b: keep track of x-product of last connected points
     for p1, p2, _ in closest_pairs[slice(None, 1_000 if part == "a" else None)]:
         if p1 not in clusters[p2]:
-            # merge and update all cluster references
-            c = clusters[p1] | clusters[p2]
-            for p in c:
-                clusters[p] = c
-            last_product = p1.i * p2.i  # part b
+            # merge into p1 clusters
+            clusters[p1].update(clusters[p2])
+            # update all cluster references in p2
+            for p in clusters[p2]:
+                clusters[p] = clusters[p1]
+            last_product = p1[0] * p2[0]  # part b
 
     # part b
     if part == "b":
