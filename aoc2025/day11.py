ba749722647b7c86f369052a37e6c871d07a9042
Marcel R. <github.riga@icloud.com>
2025-12-11 08:34:43 +0100

    Additional day 11 solution.

--- aoc2025/day11.py
+++ aoc2025/day11.py
@@ -41,6 +41,32 @@ def solution(data: list[str], part: Part) -> int | str | None:
     return count_valid_paths("svr", False, False)
 
 
+def solution_simpler(data: list[str], part: Part) -> int | str | None:
+    # build graph
+    devices: dict[str, set[str]] = {
+        parts[0][:-1]: set(parts[1:])
+        for parts in (line.split() for line in data)
+    }
+
+    # dp helper to count paths between two nodes
+    @functools.cache
+    def count_paths(start: str, end: str) -> int:
+        if start == end:
+            return 1
+        return sum(count_paths(n, end) for n in devices.get(start, []))
+
+    # part a: simple count between nodes
+    if part == "a":
+        return count_paths("you", "out")
+
+    # part b: break down into segments and combine, summing over all options (svr-fft-dac-out and svr-dac-fft-out)
+    return (
+        count_paths("svr", "fft") * count_paths("fft", "dac") * count_paths("dac", "out") +
+        count_paths("svr", "dac") * count_paths("dac", "fft") * count_paths("fft", "out")
+    )
+
+
 if __name__ == "__main__":
     solver = Solver(year=2025, day=11, truth_a=497, truth_b=358_564_784_931_864)
-    solver(solution, part="x", submit=False)
+    # solver(solution, part="x", submit=False)
+    solver(solution_simpler, part="x", submit=False)
